b40b926b146143268f4863c0ff13b027b4d8eedd
daniemeas <danie.meas@gmail.com>
2019-02-26 22:09:22 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+var firstName ='John';
+var age = 28;
+console.log(firstName +' '+ age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+age = 'twenty eight'
+job = 'driver'
+
+alert(firstName + ' is a ' + age + ' year old ' + job + ' is he married?' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+
+/*Basic Operators*/
+
+var year, yearJohn, yearMark;
+now = 2020;
+ageJohn = 28;
+ageMark = 33;
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+/* Logical Operators */
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
